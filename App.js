c721990a9dc597037a2b9d9da6c5509309600942
Miroslav <miki.skela90@gmail.com>
2020-11-05 04:53:22 -0600

    first commit

--- App.js
+++ App.js
@@ -1,15 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import * as Location from "expo-location";
+import * as Permissions from 'expo-permissions';
+import * as TaskManager from 'expo-task-manager'
 export default function App() {
+  useEffect(() => {
+    const trackLocation = async () => {
+      const { status } = await Permissions.askAsync(Permissions.LOCATION);
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+        return;
+      }
+      Location.startLocationUpdatesAsync("TRACK_LOCATION", { accuracy: Location.Accuracy.High, distanceInterval: 2, timeInterval: 2000, showsBackgroundLocationIndicator: true })
+      let hasStarted = await Location.hasStartedLocationUpdatesAsync("TRACK_LOCATION")
+      console.log(hasStarted)
+    };
+    trackLocation();
+    
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Your location</Text>
     </View>
   );
 }
+
+TaskManager.defineTask(
+  "TRACK_LOCATION",
+  async ({ data: { locations }, error }) => {
+    if (error) {
+      console.log(error);
+      // check `error.message` for more details.
+      return;
+    }
+    console.log("locations/////")
+    console.log("*******")
+    console.log(locations[0])
+    
+  }
+);
 
 const styles = StyleSheet.create({
   container: {
